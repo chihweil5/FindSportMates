89c79ea8e7653ef404b00dad7c18cb530375d741
Chou Yi <yich8573@colorado.edu>
2016-11-17 02:55:32 -0700

    Add files via upload

--- FindSportMates_ChouYi/FindSportMates/src/com/FindSportMates/Event.java
+++ FindSportMates_ChouYi/FindSportMates/src/com/FindSportMates/Event.java
@@ -19,11 +19,67 @@ public class Event {
 	
 	@Column(name = "type")
 	private String type;
-	private String time;
+	@Column(name = "dateTime")
+	private String mysqlDateString;
+	@Column(name = "place")
+	private String place;
+	@Column(name = "createrId")
+	private int createrId;
+
 	public Event(){
 		
 	}
+	public Event(String _type, String _dateTime, String _place, int _createrId)
+    {
+        this.type = _type;
+        //this.mysqlDateString=_dateTime;
+        setMysqlDateString(_dateTime);
+        this.place = _place;
+        this.createrId=_createrId;
 
+    }
+	public String getType() {
+		return type;
+	}
+
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+
+	public String getMysqlDateString() {
+		return mysqlDateString;
+	}
+
+
+	public void setMysqlDateString(String pattern) {
+		Date now = new Date();
+        //note String pattern = "yyyy-MM-dd";
+        SimpleDateFormat formatter = new SimpleDateFormat(pattern);
+		this.mysqlDateString = formatter.format(now);
+		//this.mysqlDateString = pattern;
+	}
+
+
+	public String getPlace() {
+		return place;
+	}
+
+
+	public void setPlace(String place) {
+		this.place = place;
+	}
+
+
+	public int getCreaterId() {
+		return createrId;
+	}
+
+
+	public void setCreaterId(int createrId) {
+		this.createrId = createrId;
+	}
 
 	public int getId() {
 	      return id;
--- FindSportMates_ChouYi/FindSportMates/src/com/FindSportMates/SearchEvent.java
+++ FindSportMates_ChouYi/FindSportMates/src/com/FindSportMates/SearchEvent.java
@@ -1,11 +1,42 @@
 package com.FindSportMates;
 
-public class SearchEvent extends Search{
+import org.hibernate.cfg.Configuration;
 
+import java.util.Iterator;
+import java.util.List;
+
+import org.hibernate.Query;
+import org.hibernate.Session; 
+import org.hibernate.cfg.Configuration;
+//import org.hibernate.cfg.*;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+
+public class SearchEvent {
+	private static SessionFactory sessionfactory;
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		sessionfactory = new Configuration().configure().buildSessionFactory();
+		SearchEvent ME = new SearchEvent();
+		////datetime = "yyyy-MM-dd";
+		ME.addEvent("basketball","2016-11-03","room1",2);
+
+		sessionfactory.close(); 
 
 	}
+	/* Method to CREATE an event in the database */
+	public void addEvent(String _type, String _dateTime, String _place, int _createrId){
+
+		
+	    Event event = new Event(_type,_dateTime,_place,_createrId);
+
+	    Session session = sessionfactory.openSession();
+	    Transaction tx= session.beginTransaction();
+	    session.save(event);
+	    tx.commit();
+
+	}
+	
 	public String getInfo(){
 	return "0";
 	}
