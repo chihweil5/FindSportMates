ee89d8575369f5bfe33e34e34813d8bbb5f5fd47
Yu-Chih (Joyce) Cho <aejh82081@gmail.com>
2016-12-05 11:38:39 -0700

    update user service for register

--- FindSportMates_chihwei/findsportmates/src/main/java/com/findsportmates/service/UserService.java
+++ FindSportMates_chihwei/findsportmates/src/main/java/com/findsportmates/service/UserService.java
@@ -5,7 +5,6 @@ import java.util.List;
 import com.findsportmates.model.User;
 
 public interface UserService {
-	
 	public void addUser(User u);
 	public void updateUser(User u);
 	public List<User> listUsers();
