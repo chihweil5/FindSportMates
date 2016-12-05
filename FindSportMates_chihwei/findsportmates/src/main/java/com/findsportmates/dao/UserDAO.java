d9c9c39e29ca198907fdf72966c5ae012d2158ac
Yu-Chih (Joyce) Cho <aejh82081@gmail.com>
2016-12-05 11:37:55 -0700

    update userdao.java

--- FindSportMates_chihwei/findsportmates/src/main/java/com/findsportmates/dao/UserDAO.java
+++ FindSportMates_chihwei/findsportmates/src/main/java/com/findsportmates/dao/UserDAO.java
@@ -5,7 +5,6 @@ import java.util.List;
 import com.findsportmates.model.User;
 
 public interface UserDAO {
-	
 	public void addUser(User u);
 	public void updateUser(User u);
 	public List<User> listUsers();
